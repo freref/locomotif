29e81524675c3d0cc4d39e6ecde25e415d87022a
freref <35976402+freref@users.noreply.github.com>
2025-10-26 20:29:02 +0100

    uncomment lower bound

--- locomotif/loco_jit.py
+++ locomotif/loco_jit.py
@@ -167,8 +167,8 @@ def find_best_paths(csm, mask, tau, l_min=10, vwidth=5, warping=True):
     # Mask all zeros
     mask = mask | (csm <= 0)
     
-    # min_path_length = l_min if not warping else np.ceil(l_min / 2)
-    start_mask = (~mask) # & (csm >= tau * min_path_length)
+    min_path_length = l_min if not warping else np.ceil(l_min / 2)
+    start_mask = (~mask) & (csm >= tau * min_path_length)
     
     pos_i, pos_j = np.nonzero(start_mask)
     
